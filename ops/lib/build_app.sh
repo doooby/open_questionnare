b2c8b4b4708ca637d1b0b4ce8d76379df330aa3f
d3oby <zelazk.o@email.cz>
2019-11-29 15:05:18 +0100

    ops ver2.10

--- ops/lib/build_app.sh
+++ ops/lib/build_app.sh
@@ -3,6 +3,6 @@ set -e
 ops_dir=$1
 
 docker build -t app:base -f $ops_dir/app.base.Dockerfile ..
-docker run --rm -v $(pwd)/../var/bundle:/usr/local/bundle/ app:base bundle install --without development test --clean
+docker run --rm -v $(pwd)/../var/bundle:/usr/local/bundle/ app:base bundle install --without development test --clean --quiet
 #docker run --rm -v $(pwd)/../var/node_modules:/app/node_modules app:base yarn install
 docker build -t app:build -f $ops_dir/app.Dockerfile ..
