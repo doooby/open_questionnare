75be2b66b4077d1f975053e9233784065d831d59
d3oby <zelazk.o@email.cz>
2019-12-15 17:22:46 +0100

    ops v4.7

--- ops/lib/build_app.sh
+++ ops/lib/build_app.sh
@@ -12,8 +12,8 @@ docker build -t app:production -f ops/docker/app.production.Dockerfile $lib_path
 echo "running: build rails assets"
 docker run --rm -v $lib_path/www:/var/www -v $lib_path/assets:/app/public/assets app:production bash -c "cp -R public/* /var/www && bin/rails assets:precompile assets:clean"
 
-echo "running: assets & frontend build"
-docker run --rm -v $lib_path/frontend/build:/app/public -v $lib_path/frontend:/var/frontend -v $lib_path/frontend/node_modules:/app/app/frontend/node_modules app:production bash -c "cp -R public/* /var/www && bash ops/lib/frontend/build.sh && cp -R public/* /var/www"
+echo "running: frontend build"
+docker run --rm -v $lib_path/frontend/build:/app/public -v $lib_path/frontend:/var/frontend -v $lib_path/frontend/node_modules:/app/app/frontend/node_modules app:production bash -c "rm -rf app/public/* && bash ops/lib/frontend/build.sh"
 
 echo "building: app"
 docker build -t app:build -f ops/docker/app.build.Dockerfile $lib_path
