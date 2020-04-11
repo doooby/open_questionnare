09210341c5f4f806e207c734145d359b86a0fbcc
d3oby <zelazk.o@email.cz>
2020-04-11 20:58:06 +0200

    ditched backend assets

--- ops/lib/build_release_images.sh
+++ ops/lib/build_release_images.sh
@@ -4,8 +4,7 @@ source src/ops/lib/libops.sh
 mnt_app="-v $stack_path/src:/app"
 mnt_gems="-v $stack_path/var/ruby_bundle:/usr/local/bundle"
 mnt_node_modules="-v $stack_path/var/node_modules:/app/node_modules"
-mnt_assets_backend="-v $stack_path/var/assets_backend:/app/public/assets"
-mnt_assets_frontend="-v $stack_path/var/assets_frontend:/app/frontend/build"
+mnt_fronted="-v $stack_path/var/fronted:/app/frontend/build"
 
 libops_print "release prerequisites" "title"
 
@@ -19,14 +18,9 @@ libops_docker_run app:base \
     "$mnt_app $mnt_node_modules" \
     "bin/frontend install --silent"
 
-libops_print "compiling backend assets"
-libops_docker_run app:base \
-    "$mnt_app $mnt_gems $mnt_node_modules $mnt_assets_backend" \
-    "bin/rails assets:precompile assets:clean"
-
 libops_print "compiling frontend"
 libops_docker_run app:base \
-    "$mnt_app $mnt_node_modules $mnt_assets_frontend" \
+    "$mnt_app $mnt_node_modules $mnt_fronted" \
     "bin/frontend build --silent --clean"
 
 src/ops/bin/rebuild_image app release
